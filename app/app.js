6c3a0dec67ee8ae0f5b6bfd91403e1188799a0f2
Armenouhi <armenuhitoroyan1993@gmail.com>
2018-07-16 00:04:33 +0400

    AngularJS menu

--- app/app.js
+++ app/app.js
@@ -6,6 +6,11 @@ angular.module('myApp', [
   'myApp.view1',
   'myApp.view2',
   'myApp.anothererr',
+  'myApp.services',
+  'myApp.work',
+    'myApp.about',
+    'myApp.blog',
+    'myApp.contact',
   'myApp.version'
 ]).
 config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {

92e30c0562b3c2ce9546708ffd2d04beef529bed
unknown <armenuhitoroyan1993@gmail.com>
2018-08-14 22:19:56 +0400

    Angular with nodejs

--- Angular Project/app/cars/cars.js
+++ Angular Project/app/cars/cars.js
@@ -14,16 +14,5 @@ angular.module('myApp.cars', ['ngRoute'])
         .then(function(response) {
             $scope.carss = response.data;
 
-            $scope.carsImg = {
-                "width" : "220px",
-                "height" : "100px",
-                "margin-top" : "13px"
-            };
-
-            $scope.IsVisible = true;
-
-            $scope.ShowHide = function(){
-                $scope.IsVisible = $scope.IsVisible = false;
-            }
         });
 });
